98cc6f942450c2f5dd508cd201af495c63cb2d2e
Oma10ar <omar19mousa@gmail.com>
2026-03-14 04:49:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculator_Project
+{
+    public   class Setting
+    {
+        public static int DayNumber
+        {
+           get
+            {
+                return DateTime.Today.Day;
+            }
+        
+        }  
+        public static string DayName
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+        private Setting()
+        {
+
+        }
+    }
+   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine( Setting.DayNumber);
+            Console.WriteLine( Setting.DayName);
+        }
+    }
+}
+
